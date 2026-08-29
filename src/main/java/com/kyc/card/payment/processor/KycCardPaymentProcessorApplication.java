b4f3dfda948b54706ec65f6852596c2adbd62364
LHPH <luislyly@hotmail.com>
2026-08-28 21:57:43 -0600

    Upload template for this project

---
+++ src/main/java/com/kyc/card/payment/processor/KycCardPaymentProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.kyc.card.payment.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycCardPaymentProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KycCardPaymentProcessorApplication.class, args);
+    }
+
+}
